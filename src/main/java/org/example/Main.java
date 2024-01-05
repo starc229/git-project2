142e2fb3d9fa8eab7e65326bf1fa038beeed0480
starc229 <hs527774@gmail.com>
2024-01-06 01:25:58 +0530

    [Version 1.1] bubblesort program

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,30 @@ package org.example;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] arr = {6,4,5,2,7,8,1,9};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    static void bubble(int[] arr){
+        for (int i = 0; i <arr.length ; i++) {
+            boolean swap = false;
+            for (int j = 1; j <arr.length - i ; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    swap = true;
+                }
+            }
+            if(!swap){
+                break;
+            }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("Hey guys");
         }
     }
 }
